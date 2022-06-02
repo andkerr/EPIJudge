c61ed16b6edf061e5de30480c36ca91f5ed98bfd
Andrew Kerr <kerrand@protonmail.com>
2022-06-02 14:47:37 -0400

    pow_x_y optimized

--- epi_judge_cpp/power_x_y.cc
+++ epi_judge_cpp/power_x_y.cc
@@ -1,16 +1,59 @@
 #include "test_framework/generic_test.h"
 #include <cmath>
 
-// given: a double x, and and integer y
-// unknown: the double value that represents x raised to the y-th power
+// recursive
+//
+// double Power(double x, int y) {
+//   if (y == 0) {
+//     return 1.0;
+//   }
+//
+//   if (y < 0) {
+//     y = -y;
+//     x = 1 / x;
+//   }
+//
+//   double partial = Power(x, y >> 1);
+//   if (y & 1) {
+//     return partial * partial * x;
+//   }
+//   else {
+//     return partial * partial;
+//   }
+// }
+
+// optimized iterative
+//
 double Power(double x, int y) {
   double res = 1.0;
+  long long power = y; // guard against y == INT_MIN
 
-  for (int i = 0; i * i < y * y; ++i) {
-    res *= (y < 0) ? (1.0 / x) : x;
+  if (power < 0) {
+    power = -power;
+    x = 1 / x;
   }
+
+  while (power) {
+    if (power & 1) {
+      res *= x;
+    }
+    x *= x;
+    power >>= 1;
+  }
+
   return res;
 }
+
+// naive
+//
+// double Power(double x, int y) {
+//   double res = 1.0;
+//
+//   for (int i = 0; i * i < y * y; ++i) {
+//     res *= (y < 0) ? (1.0 / x) : x;
+//   }
+//   return res;
+// }
 
 int main(int argc, char* argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
