dc51565c6c37a76c7a8cb9964b3fb9722b2f4a6a
Andrew Kerr <kerrand@protonmail.com>
2022-06-11 11:38:19 -0400

    solve grade_school_multiply

--- epi_judge_cpp/int_as_array_multiply.cc
+++ epi_judge_cpp/int_as_array_multiply.cc
@@ -2,9 +2,40 @@
 
 #include "test_framework/generic_test.h"
 using std::vector;
+
 vector<int> Multiply(vector<int> num1, vector<int> num2) {
-  // TODO - you fill in here.
-  return {};
+  // naive solution - work out negative signs up front
+  if ((num1 == vector<int> {0}) ||
+      (num2 == vector<int> {0})) {
+    return vector<int> {0};
+  }
+
+  bool neg = (num1[0] < 0) ^ (num2[0] < 0);
+  num1[0] = (int) abs(num1[0]);
+  num2[0] = (int) abs(num2[0]);
+
+  int tmp;
+  int carry;
+  vector<int> res(num1.size() + num2.size());
+  for (int n = num1.size(); n > 0; --n) {
+    carry = 0;
+    for (int m = num2.size(); m > 0; --m) {
+      tmp = num1[n - 1] * num2[m - 1] + res[n + m - 1] + carry;
+      res[n + m - 1] = tmp % 10;
+      carry = (int) tmp / 10;
+    }
+    res[n - 1] = carry;
+  }
+
+  if (res[0] == 0) {
+    res.erase(res.begin());
+  }
+
+  if (neg) {
+    res[0] *= -1;
+  }
+
+  return res;
 }
 
 int main(int argc, char* argv[]) {
