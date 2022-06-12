2a704c86458759dfada3bef783100658df04ef45
Andrew Kerr <kerrand@protonmail.com>
2022-06-11 20:41:17 -0400

    solve apply_perm

--- epi_judge_cpp/apply_permutation.cc
+++ epi_judge_cpp/apply_permutation.cc
@@ -1,9 +1,34 @@
+#include <unordered_set>
 #include <vector>
 
 #include "test_framework/generic_test.h"
 using std::vector;
+using std::unordered_set;
 void ApplyPermutation(vector<int> perm, vector<int>* A_ptr) {
-  // TODO - you fill in here.
+  vector<int>& A = *A_ptr;
+  unordered_set<int> visited;
+
+  int idx = 0;
+  int cycle, tmp1, tmp2;
+  while (visited.size() < A.size()) {
+    if (visited.find(idx) != visited.end()) {
+      ++idx;
+    }
+    else { // we have reached an unvisited index, let's begin a cycle here
+      cycle = idx;
+      tmp1 = A[cycle];
+      while (perm[cycle] != idx) {
+        tmp2 = A[perm[cycle]];
+        A[perm[cycle]] = tmp1;
+        tmp1 = tmp2;
+        cycle = perm[cycle];
+        visited.insert(cycle);
+      }
+      A[idx] = tmp1;
+      visited.insert(idx);
+    }
+  }
+
   return;
 }
 vector<int> ApplyPermutationWrapper(const vector<int>& perm, vector<int> A) {
